100c8d1f6402f62e095476e34def2ea67c8d5ca7
John Crouch <john.crouch@teamaol.com>
2016-04-27 17:56:23 -0400

    pretty header in uglified file ooh la la

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,20 @@
+var jsHeader = "/*                                  \n\
+	 _ _      _       _                             \n\
+ ___| (_) ___| | __  (_)___                         \n\
+/ __| | |/ __| |/ /  | / __|                        \n\
+\__ \ | | (__|   < _ | \__ \                        \n\
+|___/_|_|\___|_|\_(_)/ |___/                        \n\
+				   |__/                             \n\
+                                                    \n\
+ Version: 1.5.9                                     \n\
+  Author: Ken Wheeler                               \n\
+ Website: http://kenwheeler.github.io               \n\
+	Docs: http://kenwheeler.github.io/slick         \n\
+	Repo: http://github.com/kenwheeler/slick        \n\
+  Issues: http://github.com/kenwheeler/slick/issues \n\
+                                                    \n\
+ */\n\n";
+
 module.exports = function(grunt) {
 
 	// Project configuration.
@@ -6,7 +23,7 @@ module.exports = function(grunt) {
 
 		uglify: {
 			options: {
-				// banner: jsHeader
+				banner: jsHeader
 			},
 			build: {
 				files: {
